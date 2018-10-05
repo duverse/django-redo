9ec8440316a6092cac4903f49ca60c77efb2dbca
duverse <geany.been@gmail.com>
2018-10-05 11:48:46 +0300

    Fixed setup py.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+import setuptools
 from distutils.core import setup
 
 
 setup(
     name='Django ReDo',
     version='0.1alpha',
-    packages=[
-        'django_redo',
-    ],
+    packages=setuptools.find_packages(),
+    url='https://github.com/duverse/django-redo',
     license='General Public License v3.0',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
 )
